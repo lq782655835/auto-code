47d6e4b4f8786bef205b79ffaa2a6b94e0787856
springleo <782655835@qq.com>
2019-07-24 19:49:55 +0800

    :heavy_plus_sign:	Add doT as template engine

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const Handlebars = require('handlebars');
+const doT = require('dot')
 const fs = require('fs-extra')
 const data = require('./config.json')
 const util = require('./util')
 const readFile = require('util').promisify(fs.readFile);
+doT.templateSettings.strip = false
 
-let init =  async () => {
-    let file = await readFile(util.path('./template/server.ts'), {encoding:"utf-8"})
-    var template = Handlebars.compile(file);
-    var result = template(data);
-    console.log(result)
-}
-init()
+// 自解析制定目录下.jst后缀
+// http://jinlong.github.io/doT/
+doT.process({path: util.path('./template')});
+var pageFunc = require('./template/server')
+var result = pageFunc(data)
+console.log(result)
+
+// let init =  async () => {
+//     let file = await readFile(util.path('./template/server.ts'), {encoding:"utf-8"})
+//     var template = doT.template(file);
+//     var result = template(data);
+//     console.log(result)
+// }
+// init()
